0fb07fae711cfe0d221b6d5698f9be0db35d91e9
Omar Hammoud <omar.s.hammoud@outlook.com>
2022-12-14 21:31:27 +0200

    Tested saving to database

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,9 +1,13 @@
 // ====================================
 //             Requirements
 // ====================================
-const express = require('express');
+const express = require("express");
 const mainRouter = express.Router();
 const dotenv = require("dotenv");
+const moment = require("moment");
+
+// Project Made Module
+const DailyProgress = require("../database/dailyProgress");
 
 // ====================================
 //             Configuration
@@ -18,8 +22,38 @@ dotenv.config({
 //          Main
 //========================
 
-mainRouter.get('/add', (req, res, next) => {
-	res.render('add');
+mainRouter.get("/add", (req, res, next) => {
+	res.render("main/add");
+});
+
+mainRouter.post("/add", async (req, res, next) => {
+	let dateToday = new Date().setHours(0, 0, 0, 0);
+	DailyProgress.findOne({ day: dateToday })
+		.then((data) => {
+			if (data == null) {
+				let dailyProgress = new DailyProgress({
+					day: dateToday,
+				});
+				dailyProgress
+					.save()
+					.then(() => {})
+					.catch((err) => {
+						next(err);
+					});
+				res.redirect("/add");
+			} else res.redirect("/add");
+		})
+		.catch((err) => {
+			next(err);
+		});
+	// dailyProgress
+	// 	.save()
+	// 	.then(() => {
+	// 		res.redirect("/add");
+	// 	})
+	// 	.catch((err) => {
+	// 		next(err);
+	// 	});
 });
 
 //========================
