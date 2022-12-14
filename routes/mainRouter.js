1f07107b8caa6d944fce23328c67ca5e54985a5b
Omar Hammoud <omar.s.hammoud@outlook.com>
2022-12-14 20:08:05 +0200

    Add view files

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,13 +3,23 @@
 // ====================================
 const express = require('express');
 const mainRouter = express.Router();
+const dotenv = require("dotenv");
+
+// ====================================
+//             Configuration
+// ====================================
+
+// dotenv
+dotenv.config({
+	path: "./config/config.env", // Specifies dotenv
+});
 
 //========================//
 //          Main
 //========================//
 
-mainRouter.get('/', (req, res, next) => {
-	res.render('main/home');
+mainRouter.get('/add', (req, res, next) => {
+	res.render('add');
 });
 
 //========================//
