8c3e6df75f9e3de8cbf90fd53889645838e6ef23
Omar Hammoud <omar.s.hammoud@outlook.com>
2022-12-19 20:41:38 +0200

    Added a list

--- views/helpers/handlebarsHelper.js
+++ views/helpers/handlebarsHelper.js
@@ -1,8 +1,8 @@
-const handlebars = require('express-handlebars');
-const moment = require('moment');
+const handlebars = require("express-handlebars");
+const moment = require("moment");
 
 hbs = handlebars.create({
-	defaultLayout: 'main',
+	defaultLayout: "main",
 	runtimeOptions: {
 		allowProtoPropertiesByDefault: true,
 		allowProtoMethodsByDefault: true,
@@ -14,6 +14,22 @@ hbs = handlebars.create({
 			} else {
 				return false;
 			}
+		},
+		calculateHours: function (hoursWorkedArr) {
+			if (!hoursWorkedArr) return;
+			
+			let count = 0;
+			
+			for (i = 0; i < hoursWorkedArr.length; i++) {
+				console.log("from " + moment(hoursWorkedArr.from).format("dddd, MMMM Do YYYY, hh:mm"))
+				count += (hoursWorkedArr[i].to.getTime() - hoursWorkedArr[i].from.getTime())/1000; //this gets time in seconds
+			}
+
+			return count/3600;
+		},
+		presentTime: function (date) {
+			if (!date) return;
+			return moment(date).format("dddd, MMMM Do YYYY");
 		},
 	},
 });
