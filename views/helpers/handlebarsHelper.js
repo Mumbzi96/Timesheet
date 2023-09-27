2e797390060bdc0019620fc9a2db1b65c9f64b5d
Omar Hammoud <omar.s.hammoud@outlook.com>
2023-09-27 11:17:05 +0300

    Updated design for lists page

--- views/helpers/handlebarsHelper.js
+++ views/helpers/handlebarsHelper.js
@@ -17,19 +17,25 @@ hbs = handlebars.create({
 		},
 		calculateHours: function (hoursWorkedArr) {
 			if (!hoursWorkedArr) return;
-			
+
 			let count = 0;
-			
+
 			for (i = 0; i < hoursWorkedArr.length; i++) {
-				console.log("from " + moment(hoursWorkedArr.from).format("dddd, MMMM Do YYYY, hh:mm"))
-				count += (hoursWorkedArr[i].to.getTime() - hoursWorkedArr[i].from.getTime())/1000; //this gets time in seconds
+				console.log(
+					"from " +
+						moment(hoursWorkedArr.from).format("dddd, MMMM Do YYYY, hh:mm")
+				);
+				count +=
+					(hoursWorkedArr[i].to.getTime() - hoursWorkedArr[i].from.getTime()) /
+					1000; //this gets time in seconds
 			}
 
-			return count/3600;
+			return count / 3600;
 		},
-		presentTime: function (date) {
+		presentTime: function (date, getTime) {
 			if (!date) return;
-			return moment(date).format("dddd, MMMM Do YYYY");
+			if (!getTime) return moment(date).format("dddd, MMMM Do YYYY");
+			else return moment(date).format("h:mm A");
 		},
 	},
 });
