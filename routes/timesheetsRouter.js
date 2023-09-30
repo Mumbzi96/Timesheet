934329ee870a27962ee8e10b1706bee83873447d
Omar Hammoud <omar.s.hammoud@outlook.com>
2023-09-30 10:26:46 +0300

    added a calendar view

--- routes/timesheetsRouter.js
+++ routes/timesheetsRouter.js
@@ -20,14 +20,48 @@ const Project = require("../database/schemas-models/projects.js");
 
 timesheetsRouter.get("/", async (req, res, next) => {
 	// Get daily progress
-	let data = await DailyProgress.find({}).populate("hoursWorked.projectWorkedOn").exec();
+	let data = await DailyProgress.find({})
+		.populate("hoursWorked.projectWorkedOn")
+		.exec();
 
 	// Render
-	res.render("main/list", {
+	res.render("main/listCalendar", {
 		data,
 		flyingIcon: "fa-solid fa-plus",
 		pageToFlyTo: "/timesheets/add",
 	});
+});
+
+//========================
+//          json
+//========================
+
+timesheetsRouter.get("/calendar", async (req, res, next) => {
+	// Get daily progress
+	let data = await DailyProgress.find({})
+		.populate("hoursWorked.projectWorkedOn")
+		.exec();
+
+	// setup events
+	const events = [];
+
+	// looping each day
+	data.forEach((eventData) => {
+		const dailyEvents = eventData.hoursWorked;
+		// looping every hoursWorked value to add as an event
+		dailyEvents.forEach((dailyEvent) => {
+			const event = {
+				// id: eventData._id,
+				title: dailyEvent.projectWorkedOn.name,
+				start: dailyEvent.from.toISOString(), // Use the "day" field as the start date/time
+				end: dailyEvent.to.toISOString(), // Use the "day" field as the start date/time
+				// Add more properties as needed
+			};
+			events.push(event);
+		});
+	});
+	
+	res.json(events);
 });
 
 //========================
